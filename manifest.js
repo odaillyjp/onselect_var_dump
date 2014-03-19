dcfcd148775c8e0dd9fd981e668b8dee29ae6915
Suguru Odai <ourqwers@gmail.com>
2014-03-20 01:14:38 +0900

    Add manifest

---
+++ manifest.js
@@ -0,0 +1,10 @@
+{
+  "manifest_version": 2,
+  "name"            : "add_var_dump",
+  "version"         : "1.0",
+  "description"     : "選択部分を var_dump 関数で囲う機能を追加します。",
+  "permissions"     : ["contextMenus", "tabs", "http://sandbox.onlinephpfunctions.com/"],
+  "background": {
+    "scripts"       : ["background.js"]
+  }
+}
